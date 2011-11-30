6a90c8fab64a39f35f826eafe6196997af9622b0
dpletter <dpletter@salesforce.com>
2011-11-30 14:00:26 -0800

    search stuff

--- src/main/java/org/lumenframework/demo/notes/HelloNotes.java
+++ src/main/java/org/lumenframework/demo/notes/HelloNotes.java
@@ -6,7 +6,9 @@ import java.util.Date;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcDatabaseConnection;
 import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
+import com.j256.ormlite.stmt.StatementBuilder.StatementType;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
