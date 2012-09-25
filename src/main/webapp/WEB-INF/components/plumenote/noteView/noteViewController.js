1815c5dfcc025487ccd3bf57ce87357767b4aefd
Trevor Bliss <tbliss@salesforce.com>
2012-09-24 17:18:43 -0700

    Changed $L to $P

--- src/main/webapp/WEB-INF/components/plumenote/noteView/noteViewController.js
+++ src/main/webapp/WEB-INF/components/plumenote/noteView/noteViewController.js
@@ -2,7 +2,7 @@
     edit: function(component) {
         var note = component.getValue("v.note");
 
-        var event = $L.get("e.plumenote:openNote")
+        var event = $P.get("e.plumenote:openNote")
         event.setParams({
             mode : "edit",
             note : note
@@ -14,11 +14,11 @@
         var a = component.get("c.deleteNote");
         a.setParams({ id : component.get("v.note.id"), sort : component.get("v.sort") });
         a.setCallback(this, function(action){
-            var event = $L.get("e.plumenote:noteAdded");
+            var event = $P.get("e.plumenote:noteAdded");
             event.setParams({noteList : action.getReturnValue()});
             event.fire();
 
-            var event = $L.get("e.plumenote:openNote")
+            var event = $P.get("e.plumenote:openNote")
             event.setParams({
                 mode : "new",
             });
